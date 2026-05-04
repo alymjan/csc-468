4fbd80a959af54600bcf0b062f2ae52d266438a8
Alymjan Rejepov <ar1014524@wcupa.edu>
2026-05-03 20:26:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
 }));
 
 
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/votingApp';
+const mongoURI = process.env.MONGO_URI || 'mongodb://db:27017/votingApp';
 
 const connectDB = async () => {
     try {
